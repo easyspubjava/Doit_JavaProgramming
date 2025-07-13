d58a74d730b7ab84c7de3875ed1e021462c50ac6
easyspubjava <eunjong.park@gmail.com>
2025-07-13 14:07:39 +0900

    homework

---
+++ Homework/src/homework1/Q1.java
@@ -0,0 +1,16 @@
+package homework1;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int num = 0;
+
+        for (num = 1; ; num++) {
+            sum += num;
+            if (sum >= 500)
+                break;
+        }
+        System.out.println("num : " + num);
+        System.out.println("sun : " + sum);
+    }
+}
