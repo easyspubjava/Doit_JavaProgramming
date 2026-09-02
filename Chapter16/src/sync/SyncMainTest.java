9c2c6159f7c9c982a6246c39e714d539e6ec18c9
easyspubjava <eunjong.park@gmail.com>
2026-09-02 13:51:17 +0900

    SyncMainTest.Java 수정

--- Chapter16/src/sync/SyncMainTest.java
+++ Chapter16/src/sync/SyncMainTest.java
@@ -52,9 +52,11 @@ class Park extends Thread{
 
 class ParkWife extends Thread{
     public void run() {
-        System.out.println("출금 작업을 시작합니다. ");
-        SyncMainTest.myBank.minusMoney(1000);
-        System.out.println("minusMoney(1000) 작업이 진행되어 잔액이 " + SyncMainTest.myBank.getMoney() + "입니다.");
+        synchronized (SyncMainTest.myBank) {
+            System.out.println("출금 작업을 시작합니다. ");
+            SyncMainTest.myBank.minusMoney(1000);
+            System.out.println("minusMoney(1000) 작업이 진행되어 잔액이 " + SyncMainTest.myBank.getMoney() + "입니다.");
+        }
     }
 }
 
