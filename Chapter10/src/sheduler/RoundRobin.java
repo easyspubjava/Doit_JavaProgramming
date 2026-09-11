9184c17a2c4b2d8d5291ad692a29660698aa23e2
easyspubjava <eunjong.park@gmail.com>
2026-09-11 09:39:28 +0900

    오타 수정

--- Chapter10/src/sheduler/RoundRobin.java
+++ Chapter10/src/sheduler/RoundRobin.java
@@ -3,11 +3,11 @@ package sheduler;
 public class RoundRobin implements Scheduler {
     @Override
     public void getNextCall() {
-        System.out.println("상단 전화를 순서대로 대기열에서 가져옵니다");
+        System.out.println("상담 전화를 순서대로 대기열에서 가져옵니다");
     }
 
     @Override
     public void sendCallToAgent() {
-        System.out.println("순서대로 다음 상당원에게 배분합니다.");
+        System.out.println("순서대로 다음 상담원에게 배분합니다.");
     }
 }
